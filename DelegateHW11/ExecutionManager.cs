39795f2262296abe3a4e28240720cf14a19683cf
Artur <arturdemirchyan21@gmail.com>
2025-01-26 18:39:00 +0300

    NewPoint

---
+++ DelegateHW11/ExecutionManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateHW11
+{
+    public class ExecutionManager
+    {
+        private OperationManager _operationManager;
+        public Dictionary<Operation, Func<int>> FuncExecute { get; set; }
+        public ExecutionManager() { }
+        public ExecutionManager(OperationManager operationManager)
+        {
+            _operationManager = operationManager;
+            FuncExecute = new Dictionary<Operation, Func<int>>();
+        }
+ 
+
+        public void PopulateFunctions(Operation operation,Func<int> func)       //pass delegates and register
+        {
+            FuncExecute.Add(operation, func);
+        }
+
+ 
+
+        public void PrepareExecution()
+        {
+            PopulateFunctions(Operation.Sum, () => _operationManager.Sum());
+            PopulateFunctions(Operation.Multiply, () => _operationManager.Multiply());
+        }
+
+    }
+}
---
+++ DelegateHW11/OperationManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateHW11
+{
+    public enum Operation
+    {
+        Sum,
+        Subtract,
+        Multiply
+    }
+ 
+    public class OperationManager
+    {
+        private int _first;
+        private int _second;
+        private ExecutionManager executionManager;
+         public OperationManager(int first, int second)
+         {
+            _first = first;
+            _second = second;
+            executionManager = new ExecutionManager(this);
+            executionManager.PrepareExecution();
+
+        }
+        public int Sum()
+        {
+            return _first + _second;
+        }
+        public int Subtract()
+        {
+            return _first - _second;
+        }
+        public int Multiply()
+        {
+            return _first * _second;
+        }
+        public int Execute(Operation operation)
+        {
+            return executionManager.FuncExecute[operation]();
+        }
+ 
+
+    }
+}
